a0839cd0f8e5eff65f1edfd3b988c6895e76322f
javstore <154655656+javstore@users.noreply.github.com>
2023-12-24 19:08:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1603,14 +1603,14 @@ async def manual_filters(client, message, text=False):
                                 else:
                                     try:
                                         if settings['auto_delete']:
-                                            await asyncio.sleep(5)
+                                            await asyncio.sleep(600)
                                             await piroxrk.delete()
                                     except KeyError:
                                         grpid = await active_connection(str(message.from_user.id))
                                         await save_group_settings(grpid, 'auto_delete', True)
                                         settings = await get_settings(message.chat.id)
                                         if settings['auto_delete']:
-                                            await asyncio.sleep(5)
+                                            await asyncio.sleep(600)
                                             await piroxrk.delete()
                             except KeyError:
                                 grpid = await active_connection(str(message.from_user.id))
@@ -1644,14 +1644,14 @@ async def manual_filters(client, message, text=False):
                                 else:
                                     try:
                                         if settings['auto_delete']:
-                                            await asyncio.sleep(5)
+                                            await asyncio.sleep(600)
                                             await piroxrk.delete()
                                     except KeyError:
                                         grpid = await active_connection(str(message.from_user.id))
                                         await save_group_settings(grpid, 'auto_delete', True)
                                         settings = await get_settings(message.chat.id)
                                         if settings['auto_delete']:
-                                            await asyncio.sleep(5)
+                                            await asyncio.sleep(600)
                                             await piroxrk.delete()
                             except KeyError:
                                 grpid = await active_connection(str(message.from_user.id))
@@ -1786,6 +1786,7 @@ async def global_filters(client, message, text=False):
                                     else:
                                         try:
                                             if settings['auto_delete']:
+                                                await message.delete()
                                                 await asyncio.sleep(5)
                                                 await piroxrk.delete()
                                         except KeyError:
@@ -1793,6 +1794,7 @@ async def global_filters(client, message, text=False):
                                             await save_group_settings(grpid, 'auto_delete', True)
                                             settings = await get_settings(message.chat.id)
                                             if settings['auto_delete']:
+                                                await message.delete()
                                                 await asyncio.sleep(5)
                                                 await piroxrk.delete()
                                 except KeyError:
