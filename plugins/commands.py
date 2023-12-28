06569cd1130aa3b96bb22ffe479df9d0b6723e35
javstore <154655656+javstore@users.noreply.github.com>
2023-12-28 08:16:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,9 +18,6 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-async def about_function(client, message):
-    await message.reply_text("Need any help?\nContact Developer @devajoy")
-
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
@@ -66,8 +63,6 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-    if len(message.command) == 2 and message.command[1] == "about":
-        await about_function(client, message)
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
