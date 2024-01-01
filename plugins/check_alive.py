6910ccea0e5f24ee9cb72bcc8e267935bb73c2de
javstore <154655656+javstore@users.noreply.github.com>
2024-01-01 20:08:40 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -22,7 +22,7 @@ async def help(_, message):
 async def admin_panel(_, message):
 oii = await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME)
+            caption=script.ADMIN_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME)
 )
 
 @Client.on_message(filters.command("moviessss", CMD))
