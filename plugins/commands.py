b0da648d67a763b1212637f4f14bb67d9eb96a87
javstore <154655656+javstore@users.noreply.github.com>
2024-01-01 19:25:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,7 +33,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('🚸 𝖧𝖾𝗅𝗉', callback_data='help')
+                    InlineKeyboardButton('⚙️ 𝖧𝖾𝗅𝗉', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         oii = await message.reply_photo(
