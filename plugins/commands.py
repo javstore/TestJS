0f6b7b2a9431e0d17b9ef9514707943bf36a1d35
javstore <154655656+javstore@users.noreply.github.com>
2024-01-04 18:58:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -760,27 +760,21 @@ async def send_to_channel(bot, message):
     if message.reply_to_message:
         target_id = message.text.split(" ", 1)[1]
         try:
-            # Get channel information
-            channel = await bot.get_chat(target_id)
-            
-            # Check if the bot is an admin in the channel
-            member = await bot.get_chat_member(channel.id, bot.id)
-            
-            if member.status in ("administrator", "creator"):
-                # Send the message directly to the channel
-                await bot.send_message(channel.id, message.reply_to_message.text)
-                await message.reply_text(f"<b>Your message has been successfully sent to <code>{channel.id}</code>.</b>")
+            # Check if the bot is part of the chat (group/channel)
+            chat = await bot.get_chat(target_id)
+            if chat.type in ("supergroup", "channel", "group"):
+                await bot.send_message(chat.id, message.reply_to_message.text)
+                await message.reply_text(f"<b>Your message has been successfully sent to <code>{chat.id}</code>.</b>")
             else:
-                await message.reply_text("<b>Bot is not an admin in the specified channel!</b>")
+                await message.reply_text("<b>The provided ID doesn't correspond to a group or channel!</b>")
         except ValueError as ve:
             await message.reply_text(f"<b>ValueError: {ve}</b>")
         except ChatAdminRequired as car:
-            await message.reply_text("<b>Bot doesn't have necessary admin rights in the channel!</b>")
+            await message.reply_text("<b>Bot doesn't have necessary admin rights in the channel or group!</b>")
         except Exception as e:
             await message.reply_text(f"<b>Error: <code>{e}</code></b>")
     else:
         await message.reply_text("<b>Error: Command needs a reply to a message!</b>")
-
 
 @Client.on_message(filters.command("gsend") & filters.user(ADMINS))
 async def send_chatmsg(bot, message):
