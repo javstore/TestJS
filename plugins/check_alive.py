5a4406e34340b93bee98d94e3169d73046665de0
javstore <154655656+javstore@users.noreply.github.com>
2024-01-10 13:43:55 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -67,7 +67,7 @@ async def av_command(_, message):
                 # Send the poster as a photo
                 # Create inline buttons
                 buttons = [[
-                            InlineKeyboardButton('𝖯𝗋𝖾𝗏𝗂𝖾𝗐', url=telegraph_url)
+                            InlineKeyboardButton('𝖯𝗋𝖾𝗏𝗂𝖾𝗐', url=f"{telegraph_url}")
                 ]]
                 reply_markup = InlineKeyboardMarkup(buttons)
 
