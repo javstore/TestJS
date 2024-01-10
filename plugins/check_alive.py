dc3d7d1766a6ad43dd47154963044cec271234a6
javstore <154655656+javstore@users.noreply.github.com>
2024-01-10 15:16:33 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -74,20 +74,20 @@ async def av_command(_, message):
                         InlineKeyboardButton('𝖲𝖼𝗋𝖾𝖾𝗇𝗌𝗁𝗈𝗍𝗌', url=f"{telegraph_url}")
                     ])
                     buttons.append([
-                        InlineKeyboardButton('𝖵𝗂𝖾𝗐', url=f"{page}")
+                        InlineKeyboardButton(f'{dvd}', url=f"{page}")
                     ])
                 else:
                     buttons.append([
                         InlineKeyboardButton('𝖲𝖼𝗋𝖾𝖾𝗇𝗌𝗁𝗈𝗍𝗌', url=f"{telegraph_url}")
                     ])
                     buttons.append([
-                        InlineKeyboardButton('𝖵𝗂𝖾𝗐', url=f"{page}")
+                        InlineKeyboardButton(f'{dvd}', url=f"{page}")
                     ])
 
                 reply_markup = InlineKeyboardMarkup(buttons)
 
                 # Send the photo with caption and inline button              
-                await message.reply_photo(photo=poster, caption=f"𝖳𝗂𝗍𝗅𝖾: {title}\n𝖣𝖵𝖣 𝖨𝖣: {dvd}\n𝖦𝖾𝗇𝗋𝖾: {tags}\n𝖱𝖾𝗅𝖾𝖺𝗌𝖾 𝖣𝖺𝗍𝖾: {release_date}\n𝖱𝗎𝗇𝗍𝗂𝗆𝖾: {runtime} Minutes\n𝖠𝖼𝗍𝗋𝖾𝗌𝗌: {actresses}\n𝖣𝗂𝗋𝖾𝖼𝗍𝗈𝗋: {director}\n𝖲𝖾𝗋𝗂𝖾𝗌: {series_name_en}\n𝖲𝗍𝗎𝖽𝗂𝗈: {studio}\nTelegraph URL: {telegraph_url}\nPreview: {preview}\n\n⚠️ 𝗂𝖭𝖥𝖮 𝖻𝗒 𝖩𝖠𝖵 𝖲𝖳𝖮𝖱𝖤", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+                await message.reply_photo(photo=poster, caption=f"𝖳𝗂𝗍𝗅𝖾: {title}\n𝖣𝖵𝖣 𝖨𝖣: {dvd}\n𝖦𝖾𝗇𝗋𝖾: {tags}\n𝖱𝖾𝗅𝖾𝖺𝗌𝖾 𝖣𝖺𝗍𝖾: {release_date}\n𝖱𝗎𝗇𝗍𝗂𝗆𝖾: {runtime} Minutes\n𝖠𝖼𝗍𝗋𝖾𝗌𝗌: {actresses}\n𝖣𝗂𝗋𝖾𝖼𝗍𝗈𝗋: {director}\n𝖲𝖾𝗋𝗂𝖾𝗌: {series_name_en}\n𝖲𝗍𝗎𝖽𝗂𝗈: {studio}\n\n⚠️ 𝗂𝖭𝖥𝖮 𝖻𝗒 𝖩𝖠𝖵 𝖲𝖳𝖮𝖱𝖤", reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
             else:
                 await message.reply_text("No content ID found for the provided DVD ID")
 
