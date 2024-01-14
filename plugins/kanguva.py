b24b1b2a9e9c64b7c1dbd9d334f70937f42d29b2
javstore <154655656+javstore@users.noreply.github.com>
2024-01-14 12:04:35 +0530

    Update kanguva.py

--- plugins/kanguva.py
+++ plugins/kanguva.py
@@ -55,7 +55,7 @@ async def get_message_id(client, message):
     else:
         return 0
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@Client.on_message(filters.command("genlink") & filters.user(ADMINS))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
