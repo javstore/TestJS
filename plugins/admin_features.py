188f2f479b31651bb7b6722590ac18a6e4f8922e
javstore <154655656+javstore@users.noreply.github.com>
2024-01-18 02:37:42 +0530

    Update admin_features.py

--- plugins/admin_features.py
+++ plugins/admin_features.py
@@ -189,31 +189,3 @@ async def stop_button(bot, message):
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-# Initialize Telegraph
-telegraph = Telegraph(domain='telegra.ph')
-telegraph.create_account(short_name='JAV STORE', author_name='JAV STORE', author_url='https://telegram.me/javsub_english')
-
-# Function to post to Telegraph
-def post_to_telegraph_with_message(message):
-    text_content = f"<blockquote>Provided by JAV STORE</blockquote>"
-    text_content += f"{message}"
-    response = telegraph.create_page('Telegram Files', html_content=text_content)
-    return response['url']
-
-# Command handler for /telegraph
-@Client.on_message(filters.command("telegraph", CMD) & filters.user(ADMINS))
-async def telegraph_command(_, message):
-    # Check if there's a message attached or if there's additional text after the command
-    if message.reply_to_message and message.reply_to_message.text:
-        user_message = message.reply_to_message.text
-    elif len(message.text.split(maxsplit=1)) == 2:
-        user_message = message.text.split(maxsplit=1)[1]
-    else:
-        await message.reply_text("Please provide a message to post on Telegraph.")
-        return
-
-    # Post to Telegraph and get the URL
-    telegraph_link = post_to_telegraph_with_message(user_message)
-
-    # Send the Telegraph URL to the user
-    await message.reply_text(f"Your message has been posted on Telegraph, Here's the link: {telegraph_link}")
