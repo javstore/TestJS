59e449f52d87fda77e7852c8b5c37f044e86f23c
javstore <154655656+javstore@users.noreply.github.com>
2024-01-27 00:28:50 +0530

    Update share_text.py

--- plugins/features/share_text.py
+++ plugins/features/share_text.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from urllib.parse import quote
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command(["share_text", "share", "sharetext",]))
+@Client.on_message(filters.command(["share_text", "sharetext",]))
 async def share_text(client, message):
     reply = message.reply_to_message
     reply_id = message.reply_to_message.id if message.reply_to_message else message.id
@@ -14,9 +14,8 @@ async def share_text(client, message):
         input_text = reply.text or reply.caption
     else:
         await message.reply_text(
-            text=f"**Nᴏᴛɪᴄᴇ:**\n\n1. Rᴇᴩʟʏ Tᴏ Aɴʏ Mᴇssᴀɢᴇ.\n2. Nᴏ Mᴇᴅɪᴀ Sᴜᴩᴩᴏʀᴛ ﹝ sᴜᴩᴩᴏʀᴛs ᴏɴʟʏ ᴛᴇxᴛ ﹞\n\n**Jᴏɪɴ Nᴏᴡ Oᴜʀ Uᴩᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ**",                
+            text=f"**Nᴏᴛɪᴄᴇ:**\n\n1. Rᴇᴩʟʏ Tᴏ Aɴʏ Mᴇssᴀɢᴇ.\n2. Nᴏ Mᴇᴅɪᴀ Sᴜᴩᴩᴏʀᴛ ﹝ sᴜᴩᴩᴏʀᴛs ᴏɴʟʏ ᴛᴇxᴛ ﹞**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🙌 Uᴩᴅᴀᴛᴇs 💥", url=f"https://t.me/Tamilan_BotsZ")]])
             )                                                   
         return
     await message.reply_text(
