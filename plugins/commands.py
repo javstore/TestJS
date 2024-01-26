495402bbaa0eca3a52a73ff46dd967674696a008
javstore <154655656+javstore@users.noreply.github.com>
2024-01-26 21:25:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -625,7 +625,7 @@ async def send_channeltxtmsg(bot, message):
             chat = await bot.get_chat(target_id)
             reply_text = message.reply_to_message.text or message.reply_to_message.caption
             reply_markup = message.reply_to_message.reply_markup
-            await bot.send_message(chat.id, reply_text, disable_web_page_preview=False, parse_mode=enums.ParseMode.MARKDOWN, reply_markup=reply_markup)
+            await bot.send_message(chat.id, reply_text, reply_markup=reply_markup, disable_web_page_preview=False)
             await message.reply_text(f"<b>Your message has been successfully sent to channel <code>{chat.id}</code>.</b>")
         except Exception as e:
             await message.reply_text(f"<b>Error :- <code>{e}</code></b>")
