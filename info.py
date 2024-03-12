5d59ad4ee79dab2d5baaf8b8a1d1ff061b0e8fde
javstore <154655656+javstore@users.noreply.github.com>
2024-03-12 22:16:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/72dff2b65352ba85d
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/0593a3103ba1b9a5855bf.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1120085236').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1120085236 1621366244').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1621366244').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
