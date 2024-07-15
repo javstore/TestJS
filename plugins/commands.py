86b2768de6fe99a80812aae8737dbca2f6d24125
javstore <154655656+javstore@users.noreply.github.com>
2024-07-16 00:12:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,10 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
+
+    if message.command[1] in ["subscribe", "buy", "premium", "help", "payment"]:
+        pass
+    elif AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
