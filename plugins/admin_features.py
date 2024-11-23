89d54b4f9025e14269d227409d8e536e2fee7f15
javstore <154655656+javstore@users.noreply.github.com>
2024-11-23 22:34:32 +0530

    Update admin_features.py

--- plugins/admin_features.py
+++ plugins/admin_features.py
@@ -82,17 +82,11 @@ async def av_command(client: Client, message: Message):
         search_response.raise_for_status()
 
         search_soup = BeautifulSoup(search_response.content, 'html.parser')
-        card_container = search_soup.find("div", class_="card-container")
-        if not card_container:
-            await message.reply_text("No card container found.")
-            return
-
-        a_tag = card_container.find("a", href=True)
+        a_tag = search_soup.find('a', href=lambda x: x and '/video/' in x)
+        
         if not a_tag:
             await message.reply_text("No valid video link found.")
             return
-        #search_soup = BeautifulSoup(search_response.content, 'html.parser')
-        #a_tag = search_soup.find('a', href=lambda x: x and '/video/' in x)
 
         video_url = "https://javtrailers.com" + a_tag['href']
 
